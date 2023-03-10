fa6a1bd1251112d71d99e880dfe1e790afc3c2a3
Arshan <arshan97@github.com>
2023-03-11 03:55:46 +0530

    implemented event-bus and query service

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const {randomBytes} = require('crypto')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const axios = require("axios")
 
 const app = express()
 
@@ -14,15 +15,25 @@ app.get('/posts/:id/comments', (req, res) => {
     res.status(200).send(commentsByPostId[req.params.id] || [])
 })
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex')
     const {content} = req.body
     const comments = commentsByPostId[req.params.id] || []
     comments.push({id: commentId, content})
     commentsByPostId[req.params.id] = comments
+    await axios.post('http://localhost:4005/events', {
+        type: "CommentCreated",
+        data: {id: commentId, content, postId: req.params.id}
+    })
     res.status(201).send(comments)
 })
 
+app.post('/events', (req, res) => {
+    console.log('Received!', req.body.type)
+
+    res.send({})
+})
+
 app.listen(4001, () => {
-    console.log('Listening on port 4001')
+    console.log('Listening on 4001')
 })
