991efd5dd8a3ea0dd4eb8e351cebac57fca9dbab
Arshan <arshan97@github.com>
2023-03-12 02:44:58 +0530

    created moderation service

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,6 +12,7 @@ app.post("/events", (req, res) => {
   axios.post("http://localhost:4000/events", event).catch(err => console.log(err.message))
   axios.post("http://localhost:4001/events", event).catch(err => console.log(err.message))
   axios.post("http://localhost:4002/events", event).catch(err => console.log(err.message))
+  axios.post("http://localhost:4003/events", event).catch(err => console.log(err.message))
 
   res.send({ status: "OK!" });
 });
